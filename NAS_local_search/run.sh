bbec857df1a4074a24de7fb83a4dbb4fa4201d27
Yao Gu <yaogu2024@u.northwestern.edu>
2020-10-18 22:53:17 -0500

    update

--- NAS_local_search/run.sh
+++
@@ -1,3 +0,0 @@
-python core.py --features=60  --l1=128 --l2=160 --l3=96  --l4=448 > ninew1.log
-python core.py --features=80  --l1=384 --l2=96  --l3=160 --l4=96  > ninew2.log
-python core.py --features=100 --l1=64  --l2=448 --l3=192 --l4=352 > ninew3.log
